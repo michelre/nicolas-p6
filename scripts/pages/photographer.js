3a2b5f62484935417da7f4e2a13741c8902feb96
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-04-28 11:29:21 +0200

    affichage nom des images + likes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,74 +1,84 @@
-const fetchPhotographer = async(photographerId) => {
-    return fetch('data/photographers.json')
-        .then((res) => res.json())
-        .then(({photographers}) => photographers.find(p => p.id === photographerId));   
+const fetchPhotographer = async (photographerId) => {
+  return fetch("data/photographers.json")
+    .then((res) => res.json())
+    .then(({ photographers }) =>
+      photographers.find((p) => p.id === photographerId)
+    );
 };
-
 
 const fetchMedia = async (photographerId) => {
-    return fetch('data/photographers.json')
-        .then((res) => res.json())
-        .then(({media}) => media.filter(m => m.photographerId === photographerId));
+  return fetch("data/photographers.json")
+    .then((res) => res.json())
+    .then(({ media }) =>
+      media.filter((m) => m.photographerId === photographerId)
+    );
 };
-
 
 const displayGallery = (media) => {
-    const gallery = document.querySelector('#gallery');
-    media.forEach(element => {
-        const media = new Media(element);
-        gallery.appendChild(media.getMediaDOM());
+    const gallery = document.querySelector("#gallery");
+    media.forEach((element) => {
+      const mediaObj = new Media(element);
+      const mediaDOM = mediaObj.getMediaDOM();
+  
+      
+      const title = document.createElement("p");
+      title.textContent = element.title;
+  
+      
+      const likes = document.createElement("p");
+      likes.textContent = `${element.likes} likes`;
+  
+      
+      mediaDOM.appendChild(title);
+      mediaDOM.appendChild(likes);
+      gallery.appendChild(mediaDOM);
     });
-};
-
-
+  };
 
 const displayPhotographerInfo = (photographer) => {
-    const header = document.querySelector('.photograph-header');
-
-    
-    const name = document.createElement('h2');
-    name.textContent = photographer.name;
-    header.appendChild(name);
-
-    const location = document.createElement('p');
-    location.textContent = `${photographer.city}, ${photographer.country}`;
-    header.appendChild(location);
-
+  const header = document.querySelector(".photograph-header");
+  const namePhotographer = document.querySelector("#namePhotographer");
   
-    const tagline = document.createElement('p');
-    tagline.textContent = photographer.tagline;
-    header.appendChild(tagline);
 
-    const img = document.createElement('img');
-    img.src = `assets/photographers/${photographer.portrait}`;
-    img.alt = photographer.name;
-    header.appendChild(img);
+  const name = document.createElement("h2");
+  name.textContent = photographer.name;
+  namePhotographer.appendChild(name);
+  
+  
+  const location = document.createElement("h3");
+  location.textContent = `${photographer.city}, ${photographer.country}`;
+  namePhotographer.appendChild(location);
 
-   
-    const comma = document.createElement('span');
-    comma.textContent = ', ';
-    location.appendChild(comma);
+  const tagline = document.createElement("p");
+  tagline.textContent = photographer.tagline;
+  namePhotographer.appendChild(tagline);
+  
+
+  const img = document.createElement("img");
+  img.src = `assets/photographers/${photographer.portrait}`;
+  img.alt = photographer.name;
+  header.appendChild(img);
+  
 };
 
 const displayTotalLikes = (totalLikes) => {
-    console.log(totalLikes)
-    // HTML de la barre du bas
-}
-
-const totalLikes = (media) => {
-    return media.reduce((acc, m) => m.likes + acc, 0)
-}
-
-const init = async () => {
-    let params = new URL(document.location.toString()).searchParams;
-    let id = params.get("id");
-    const media = await fetchMedia(parseInt(id));
-    const photographer = await fetchPhotographer(parseInt(id));
-    displayGallery(media); 
-    displayPhotographerInfo(photographer); 
-    displayTotalLikes(totalLikes(media))
-    console.log(media, photographer);
+  console.log(totalLikes);
+  // HTML de la barre du bas
 };
 
+const totalLikes = (media) => {
+  return media.reduce((acc, m) => m.likes + acc, 0);
+};
+
+const init = async () => {
+  let params = new URL(document.location.toString()).searchParams;
+  let id = params.get("id");
+  const media = await fetchMedia(parseInt(id));
+  const photographer = await fetchPhotographer(parseInt(id));
+  displayGallery(media);
+  displayPhotographerInfo(photographer);
+  displayTotalLikes(totalLikes(media));
+  console.log(media, photographer);
+};
 
 init();
