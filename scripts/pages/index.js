453c44ec4a25131c915f65314a43293038025a1b
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-05-01 20:00:04 +0200

    modification css index.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,37 +1,35 @@
 async function getPhotographers() {
-        
-    return fetch('data/photographers.json')
-    .then((res) => res.json())
+  return fetch("data/photographers.json").then((res) => res.json());
 }
 
 async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+  const photographersSection = document.querySelector(".photographer_section");
 
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerTemplate(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
 
-        const locationParagraph = document.createElement('p');
-        locationParagraph.textContent = `${photographer.city}, ${photographer.country}`;
-        userCardDOM.appendChild(locationParagraph);
+    const locationParagraph = document.createElement("h4");
+    locationParagraph.textContent = `${photographer.city}, ${photographer.country}`;
+    userCardDOM.appendChild(locationParagraph);
 
+    const taglineParagraph = document.createElement("p");
+    taglineParagraph.textContent = photographer.tagline;
+    taglineParagraph.classList.add("p1");
+    userCardDOM.appendChild(taglineParagraph);
 
-        const taglineParagraph = document.createElement('p');
-        taglineParagraph.textContent = photographer.tagline;
-        userCardDOM.appendChild(taglineParagraph);
+    const priceParagraph = document.createElement("p");
+    priceParagraph.textContent = `Price: ${photographer.price} €`;
+    priceParagraph.classList.add("p2");
+    userCardDOM.appendChild(priceParagraph);
 
-        
-        const priceParagraph = document.createElement('p');
-        priceParagraph.textContent = `Price: ${photographer.price} €`;
-        userCardDOM.appendChild(priceParagraph);
-
-        photographersSection.appendChild(userCardDOM);
-    });
+    photographersSection.appendChild(userCardDOM);
+  });
 }
 
 async function init() {
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
 }
 
 init();
