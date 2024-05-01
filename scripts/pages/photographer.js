663e9bd55116ba1f01aac19894aa8171cc7495f1
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-05-02 01:11:27 +0200

    formulaire de contact ok

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -52,7 +52,7 @@ const displayGallery = (media) => {
     title.textContent = element.title;
 
     const likes = document.createElement("p");
-    likes.innerHTML = `${element.likes} <i class="heart heart-empty far fa-heart"></i><i class="heart heart-full fas fa-heart hide"></i> `; 
+    likes.innerHTML = `${element.likes} <i class="heart heart-empty far fa-heart"></i><i class="heart heart-full fas fa-heart hide"></i> `;
 
     likes.querySelector(".heart").addEventListener("click", () => {
       const emptyHeartIcon = likes.querySelector(".heart-empty");
@@ -62,17 +62,19 @@ const displayGallery = (media) => {
       if (parseFloat(emptyHeartOpacity) !== 0) {
         emptyHeartIcon.style.opacity = "0";
         filledHeartIcon.style.opacity = "1";
-        likes.childNodes[0].textContent = parseInt(likes.childNodes[0].textContent) + 1;
+        likes.childNodes[0].textContent =
+          parseInt(likes.childNodes[0].textContent) + 1;
         element.likes++;
       } else {
         emptyHeartIcon.style.opacity = "1";
         filledHeartIcon.style.opacity = "0";
         if (parseInt(likes.childNodes[0].textContent) > 0) {
-          likes.childNodes[0].textContent = parseInt(likes.childNodes[0].textContent) - 1;
+          likes.childNodes[0].textContent =
+            parseInt(likes.childNodes[0].textContent) - 1;
           element.likes--;
         }
       }
-   
+
       displayTotalLikes(totalLikes(media));
     });
 
@@ -112,13 +114,62 @@ const displayInfoWindow = (totalLikes, pricePerDay) => {
   infoWindow.style.display = "block";
 };
 
+const contactButton = document.querySelector(".contact_button");
+const modal = document.getElementById("contact_modal");
+
+// Variable pour suivre si le nom du photographe a déjà été ajouté à la modal
+let photographerNameAdded = false;
+
+function displayModal() {
+  // Vérifier si le nom du photographe a déjà été ajouté à la modal
+  if (!photographerNameAdded) {
+    // Récupérer l'ID du photographe depuis l'URL ou une autre source
+    let params = new URL(document.location.toString()).searchParams;
+    let photographerId = params.get("id");
+
+    // Appeler fetchPhotographer pour récupérer les données du photographe
+    fetchPhotographer(parseInt(photographerId))
+      .then((photographer) => {
+        // Créer un élément h3 pour afficher le nom du photographe
+        const photographerNameElement = document.createElement("h3");
+        // Ajouter le nom du photographe à l'élément h3
+        photographerNameElement.textContent = photographer.name;
+
+        // Récupérer le div avec la classe 'test'
+        const testDiv = document.querySelector(".info-form");
+        // Ajouter l'élément h3 à ce div
+        testDiv.appendChild(photographerNameElement);
+
+        // Mettre à jour la variable pour indiquer que le nom du photographe a été ajouté
+        photographerNameAdded = true;
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors de la récupération des données du photographe :",
+          error
+        );
+      });
+  }
+
+  // Afficher la modal
+  modal.style.display = "block";
+}
+
+// Fonction pour fermer la modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Ajoutez un écouteur d'événements sur le bouton "Contactez-moi"
+contactButton.addEventListener("click", displayModal);
+
 const init = async () => {
   let params = new URL(document.location.toString()).searchParams;
   let id = params.get("id");
   const media = await fetchMedia(parseInt(id));
   const photographer = await fetchPhotographer(parseInt(id));
   const totalLikesValue = totalLikes(media);
-  const pricePerDayValue = photographer.price; 
+  const pricePerDayValue = photographer.price;
   displayGallery(media);
   displayPhotographerInfo(photographer);
   displayTotalLikes(totalLikesValue);
